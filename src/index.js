1ed8398dec215dc024b2f314c705a4339d89815c
Alan <alansferreira1984@gmail.com>
2021-12-15 12:56:34 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import Button from "@mui/material/Button";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import { Grid, Container } from "@material-ui/core";
+import nunjucks from "nunjucks";
 
-function App() {
+function App(props) {
+  const [context, setContext] = useState('{"value": "hello world"}');
+  const [template, setTemplate] = useState("{{value}}");
+  const [output, setOutput] = useState("");
+
+  const handleChange = (setter, e) => setter(e.target.value);
+
+  function render() {
+    console.log(context);
+    const ctx = JSON.parse(context);
+    console.log(ctx);
+    console.log(template);
+    console.log(output);
+    nunjucks.configure({ autoescape: true });
+    const rendered = nunjucks.renderString(template, ctx);
+    console.log(rendered);
+    setOutput(rendered);
+  }
   return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+    <Container>
+      <Button onClick={render} variant="contained" color="primary">
+        Render
+      </Button>
+      <Grid container spacing={24}>
+        <Grid item xs>
+          <TextField
+            label="Context"
+            placeholder='{"value": "hello world"}'
+            fullWidth
+            multiline={true}
+            value={context}
+            onChange={(e) => handleChange(setContext, e)}
+            InputLabelProps={{
+              shrink: true
+            }}
+          />
+        </Grid>
+        <Grid item xs>
+          <TextField
+            label="Template"
+            placeholder="{{value}}..."
+            fullWidth
+            multiline={true}
+            value={template}
+            onChange={(e) => handleChange(setTemplate, e)}
+            InputLabelProps={{
+              shrink: true
+            }}
+          />
+        </Grid>
+        <Grid item xs>
+          <TextField
+            label="Output"
+            placeholder="hello world"
+            fullWidth
+            multiline={true}
+            value={output}
+            InputLabelProps={{
+              shrink: true
+            }}
+          />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
